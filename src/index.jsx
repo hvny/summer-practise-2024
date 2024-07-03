214af845303d69ec23ae7bd63f07df61f5ee573c
Vadim <elsukov.vadin@gmail.com>
2024-07-03 22:11:51 +0300

    add: history

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import { render } from 'solid-js/web';
 import './index.css';
 
 import { Route, Router } from '@solidjs/router';
+import { createHashHistory } from 'history';
+
 
 import App from './components/App/App';
 import Building from './components/Building/Building';
@@ -15,6 +17,7 @@ import TechnicalMeans from './components/TechnicalMeans/TechnicalMeans';
 import NotFoundPage from './components/NotFoundPage/NotFoundPage';
 
 const root = document.getElementById('root');
+const history = createHashHistory();
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
@@ -27,7 +30,7 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
 // ), root);
 
 render(() => (
-  <Router root={App} base={process.env.NODE_ENV === 'production' ? '/summer-practise-2024/' : '/'}>
+  <Router root={App} history={history} base={process.env.NODE_ENV === 'production' ? '/summer-practise-2024/' : '/'}>
     <Route path="/" component={Home} />
     <Route path="/building" component={Building} />
     <Route path="/disciplines" component={Disciplines} />
